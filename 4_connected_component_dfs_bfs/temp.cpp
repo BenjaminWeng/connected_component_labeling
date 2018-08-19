9e3942e7ba887ed9ce03dc871340697b99a3c238
benjamin <benjamin.weng@positivegrid.com>
2018-08-19 15:02:42 +0800

    print labels.

--- 4_connected_component_dfs_bfs/temp.cpp
+++ 4_connected_component_dfs_bfs/temp.cpp
@@ -4,7 +4,7 @@ public:
         if(grid.empty() || grid[0].empty())
             return 0;
         
-        vector<vector<char>> label(grid.size(), vector<char>(grid[0].size(), 0));
+        vector<vector<int>> label(grid.size(), vector<char>(grid[0].size(), 0));
         int res = 0;
 
         for(int i=0;i<grid.size();i++)
@@ -12,10 +12,23 @@ public:
                 if(grid[i][j]=='1' && !label[i][j])
                     bfs(i,j,grid, label, ++res);
 
+        //print labels
+        for(int i=0;i<label.size();i++)
+        {
+            for(int j=0;j<label[0].size();j++)
+            {
+                if (label[i][j] == 0)
+                    cout <<"x";
+                else
+                    cout << label[i][j];
+            }
+            cout<<"\n";
+        }
+
         return res;
     }    
     
-    void bfs(int x,int y,vector<vector<char>> &grid, vector<vector<char>> &label, int res)
+    void bfs(int x,int y,vector<vector<char>> &grid, vector<vector<int>> &label, int res)
     {
         const int dx[] = {1, 0, -1, 0};
         const int dy[] = {0, 1, 0, -1};
@@ -42,7 +55,7 @@ public:
         }
     }
 
-    void dfs(int x, int y, vector<vector<char>> &grid, vector<vector<char>> &label, int res)
+    void dfs(int x, int y, vector<vector<char>> &grid, vector<vector<int>> &label, int res)
     {
         const int dx[] = {1, 0, -1, 0};
         const int dy[] = {0, 1, 0, -1};
